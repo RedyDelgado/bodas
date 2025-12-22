e3a8460465fd4bc279886529191c8c7584f55a42
Redy <redy.delgado@gmail.com>
2025-12-21 21:25:55 -0500

    de vite fotos

--- boda-frontend/vite.config.js
+++ boda-frontend/vite.config.js
@@ -3,6 +3,11 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'import.meta.env.VITE_STORAGE_URL': JSON.stringify(
+      process.env.VITE_STORAGE_URL || 'http://localhost:8000/storage'
+    ),
+  },
   server: {
     historyApiFallback: true,
     proxy: {
